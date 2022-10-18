b25b6e747dc67f95b6ca5b76277f041f77f3caa1
Ariel Pierpont <apierpont@iconbuild.com>
2022-10-18 10:35:22 -0400

    Co-authored-by: Ariel Pierpont <apierpont@iconbuild.com> Co-authored-by: Arielle Pierpont <56093561+ariellepierpont@users.noreply.github.com>

---
+++ project/main.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
---
+++ project/userinput.js
@@ -0,0 +1,4 @@
+// gets user input for name 
+console.log(addEventListener("What is your name?"));
+
+// prints a greeting to the name 
